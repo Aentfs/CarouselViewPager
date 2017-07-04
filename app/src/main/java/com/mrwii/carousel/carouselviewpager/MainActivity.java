dead167c45becad3d3ec612cd8ad297470068857
Stefan Suhren <stefan.suhren001@stud.fh-dortmund.de>
2017-07-04 16:48:40 +0200

    Add two more infinit scroll views

--- app/src/main/java/com/mrwii/carousel/carouselviewpager/MainActivity.java
+++ app/src/main/java/com/mrwii/carousel/carouselviewpager/MainActivity.java
@@ -5,19 +5,37 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private SlideAdapter adapter;
-    private ViewPager viewPager;
+    private SlideAdapter adapter1;
+    private SlideAdapter adapter2;
+    private SlideAdapter adapter3;
+    private ViewPager viewPager1;
+    private ViewPager viewPager2;
+    private ViewPager viewPager3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = (ViewPager) findViewById(R.id.vp_slider);
-        viewPager.setClipToPadding(false);
-        viewPager.setPageMargin(10);
-        adapter = new SlideAdapter(this.getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(adapter.FIRST_PAGE);
+        viewPager1 = (ViewPager) findViewById(R.id.vp_slider1);
+        viewPager1.setClipToPadding(false);
+        viewPager1.setPageMargin(10);
+        adapter1 = new SlideAdapter(this.getSupportFragmentManager());
+        viewPager1.setAdapter(adapter1);
+        viewPager1.setCurrentItem(adapter1.FIRST_PAGE);
+
+        viewPager2 = (ViewPager) findViewById(R.id.vp_slider2);
+        viewPager2.setClipToPadding(false);
+        viewPager2.setPageMargin(10);
+        adapter2 = new SlideAdapter(this.getSupportFragmentManager());
+        viewPager2.setAdapter(adapter2);
+        viewPager2.setCurrentItem(adapter2.FIRST_PAGE);
+
+        viewPager3 = (ViewPager) findViewById(R.id.vp_slider3);
+        viewPager3.setClipToPadding(false);
+        viewPager3.setPageMargin(10);
+        adapter3 = new SlideAdapter(this.getSupportFragmentManager());
+        viewPager3.setAdapter(adapter3);
+        viewPager3.setCurrentItem(adapter3.FIRST_PAGE);
     }
 }
